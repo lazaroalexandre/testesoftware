dc97173404f540b4afd531be7767f2803584e171
lazaroalexandre <lazaroalexandre2022@gmail.com>
2024-07-27 11:16:10 -0300

    first commit

---
+++ src/main/java/com/br/homolazarusapps/testesoftware/TestesoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.br.homolazarusapps.testesoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesoftwareApplication.class, args);
+	}
+
+}
