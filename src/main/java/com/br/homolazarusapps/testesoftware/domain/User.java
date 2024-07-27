0f1e0f3bcf41cd9f261dba85fa40d89591d12c70
lazaroalexandre <lazaroalexandre2022@gmail.com>
2024-07-27 11:28:00 -0300

    first commit

---
+++ src/main/java/com/br/homolazarusapps/testesoftware/domain/User.java
@@ -0,0 +1,36 @@
+package com.br.homolazarusapps.testesoftware.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+}
